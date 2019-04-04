79ff0ff95625f60d46a34a35fd9a18277bb73d40
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-04-04 19:30:24 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -5,12 +5,17 @@ class TwitterMessage extends React.Component {
     super();
 
     this.state = {
+<<<<<<< HEAD
       tweet: ""
+=======
+      characters: 0;
+>>>>>>> 74f4076933ab7188ca1988df9a1d7df7354affee
     };
   }
 
   handleTweet = (event) => {
     this.setState({
+<<<<<<< HEAD
       tweet: event.target.value
     });
   }
@@ -19,14 +24,24 @@ class TwitterMessage extends React.Component {
     this.props.maxChars - this.state.tweet.length
   )
 
+=======
+      characters: this.props.maxChars - event.target.value.length
+    });
+  }
+
+>>>>>>> 74f4076933ab7188ca1988df9a1d7df7354affee
 
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
+<<<<<<< HEAD
         <input type="text" value={this.state.tweet} onChange={this.handleTweet}/>
         <small>{this.characters()}</small>
+=======
+        <input type="text" value={this.state.characters} onChange={this.handleTweet(event)}/>
+>>>>>>> 74f4076933ab7188ca1988df9a1d7df7354affee
       </div>
     );
   }
